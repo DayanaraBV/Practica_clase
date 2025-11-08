6e14a74458021e55c3bf0a481a878ba5804bb086
DayanaraBV <dayanara.bone@uees.edu.ec>
2025-11-08 18:47:08 -0500

    Actualizacion de variables de entorno

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const Api_Base = axios.create({
-    baseURL: 'https://api.thecatapi.com/v1',
+    baseURL: import.meta.env.VITE_CAT_API_URL,
     headers:{
-        "x-api-key": import.meta.env.VITE_CAT_API_KEY, 
-        //VITE_CAT_API_KEY es una variable de entorno que se creo para proteger la clave de la API
-        //Siempre debe estar en mayusculas y con el prefijo VITE_ para que Vite la reconozca
+        "x-api-key": import.meta.env.CAT_API_KEY, 
+        //CAT_API_KEY es una variable de entorno que se creo para proteger la clave de la API
+        //Siempre debe estar en mayusculas
         //Va en un archivo .env en la raiz del proyecto
         //Y ese archivo .env no debe ser subido al repositorio, por eso esta en el .gitignore
     }
