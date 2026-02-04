34ff4697bb897abc7074f32d6dc4ebf3e0d600b7
DayanaraBV <dayanara.bone@uees.edu.ec>
2026-02-04 18:38:04 -0500

    fix: exponer API key correctamente con prefijo VITE

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const Api_Base = axios.create({
     baseURL: import.meta.env.VITE_CAT_API_URL,
     headers:{
-        "x-api-key": import.meta.env.CAT_API_KEY, 
-        //CAT_API_KEY es una variable de entorno que se creo para proteger la clave de la API
+        "x-api-key": import.meta.env.VITE_CAT_API_KEY, 
+        //VITE_CAT_API_KEY es una variable de entorno que se creo para proteger la clave de la API
         //Siempre debe estar en mayusculas
         //Va en un archivo .env en la raiz del proyecto
         //Y ese archivo .env no debe ser subido al repositorio, por eso esta en el .gitignore
